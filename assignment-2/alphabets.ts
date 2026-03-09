bcd4bc2d8e26efef799f29350ef032f07a45d409
Deric Jojo <deric.jojo@codecraft.co.in>
2026-03-09 05:13:51 +0000

    created a array that dynamically contains a-z

--- assignment-2/alphabets.ts
+++ assignment-2/alphabets.ts
@@ -1,7 +1,8 @@
 // Generate an array containing alphabets. Then produce an object that contain two keys, ‘vowels’ and 'consonants'. The values will be array of alphabets representing vowels and consonants.
 
 const vow = ["a","e","i","o","u"];
-const alphabets = "Hello world".split("");
+// const alphabets = "Hello world".split("");
+const alphabets = Array(26).fill('').map((curr, indx) => String.fromCharCode(indx+97));
 
 const sortedObj = alphabets.reduce((acc, curr) => {
     if(vow.includes(curr.toLowerCase())) {acc.vowels.push(curr)}
