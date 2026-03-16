73ae0dbe96aac20a387f6bdc6b0038edd735b713
Deric Jojo <deric.jojo@codecraft.co.in>
2026-03-16 04:53:53 +0000

    fix: fixed unsused variables

--- assignment-3/post-browser-vite/src/main.ts
+++ assignment-3/post-browser-vite/src/main.ts
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 commentsContainer.appendChild(div);
             });
         } catch (err) {
-            commentsContainer.textContent = "Error loading comments";
+            commentsContainer.textContent = "Error loading comments: " + err;
         }
     });
 
--- assignment-3/users.test.ts
+++ assignment-3/users.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect, vi, beforeAll, afterAll } from "vitest";
-import { getUsers } from "./users";
 
 const mockUsers = [
     { id: 1, name: "Leanne Graham", username: "Bret", email: "Sincere@april.biz", address: { street: "Kulas Light", suite: "Apt. 556", city: "Gwenborough", zipcode: "92998-3874", geo: { lat: -37.3159, lng: 81.1496 } }, phone: "1-770-736-0328", website: "hildegard.org", company: { name: "Romaguera-Crona", catchPhrase: "Multi-layered client-server neural-net", bs: "harness real-time e-markets" } },
